f1f66b70ca86e8781c43f59ef07e8bd1357f207b
Kim, Jinsu <item4_hun@hotmail.com>
2015-10-16 18:20:07 +0900

    add options

--- snoin/cli.py
+++ snoin/cli.py
@@ -1,4 +1,4 @@
-from click import group
+from click import group, option
 
 from .web.app import app
 
@@ -9,8 +9,27 @@ def cli():
 
 
 @cli.command()
-def runserver():
-    app.debug = True
-    app.run()
+@option('--host', '-h')
+@option('--port', '-p', type=int)
+@option('--threaded', is_flag=True)
+@option('--processes', type=int, default=1)
+@option('--passthrough-errors', is_flag=True)
+@option('--debug/--no-debug', '-d/-D', default=None)
+@option('--reload/--no-reload', '-r/-R', default=None)
+def runserver(host, port, threaded, processes,
+              passthrough_errors, debug, reload):
+    if debug is None:
+        debug = app.debug
+    if reload is None:
+        reload = app.debug
+
+    app.run(host=host,
+            port=port,
+            debug=debug,
+            use_debugger=debug,
+            use_reloader=reload,
+            threaded=threaded,
+            processes=processes,
+            passthrough_errors=passthrough_errors)
 
 main = cli
