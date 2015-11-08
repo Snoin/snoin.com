8dbd42222600bae004769097d6b9fc6ebb7573e3
Kim Jin Su <item4_hun@hotmail.com>
2015-11-09 02:46:34 +0900

    test 의존성 추가

--- setup.py
+++ setup.py
@@ -9,11 +9,16 @@ install_requires = {
     'requests >= 2.8.1',
 }
 
+tests_require = {
+    'pytest >= 2.8.2',
+}
+
 extras_require = {
     "deploy": [
         # Deploy
         'uwsgi >= 2.0.11.2',
     ],
+    'tests': tests_require,
 }
 
 setup(
@@ -23,6 +28,7 @@ setup(
     url='http://snoin.com',
     packages=find_packages(),
     install_requires=install_requires,
+    tests_require=tests_require,
     extras_require=extras_require,
     entry_points={
         'console_scripts': [
