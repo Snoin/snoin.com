3973f2bae4bd2ca5554acafadf73f37bcb9d5662
EcmaXp <ecmaxp@ecmaxp.pe.kr>
2015-11-09 23:27:30 +0900

    loading config by runpy module.
    encoding problem should fix.

--- cfg.py.example.py
+++ cfg.py.example.py
@@ -1,7 +1,9 @@
+# -*- coding: utf-8 -*-
 import email.headerregistry
+SNOIN_CONFIG = True
 
 SMTP_ID = 'GMAIL ID'
 SMTP_PASSWORD = 'SUPER SECRET'
 MAIL_LISTENERS = [
-    email.headerregistry.Address('name', 'no-reply@snoin.com'),
+    email.headerregistry.Address('이름', 'no-reply@snoin.com'),
 ]
