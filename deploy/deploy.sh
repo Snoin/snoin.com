ecd76dbf7439f0fcc9e12a332f4a9335a325f72f
Kim Jin Su <item4_hun@hotmail.com>
2015-11-23 06:50:41 +0900

    make deploy logic

---
+++ deploy/deploy.sh
@@ -0,0 +1,6 @@
+set -e
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+    ssh -i deploy/ssh $DEPLOY_ADDR "cd /home/snoin/snoin.com && git pull -f origin master"
+    ssh -i deploy/ssh $DEPLOY_ADDR "cd /home/snoin/snoin.com && rm -rf node_modules && npm install && npm run build"
+    ssh -i deploy/ssh $DEPLOY_ADDR $DEPLOY_SERVER_RESTART
+fi
