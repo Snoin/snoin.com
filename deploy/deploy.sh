c9374a00cdd9a7c4d7fd54ee76d9700f903222e8
Kim Jin Su <item4_hun@hotmail>
2015-11-23 14:57:56 +0900

    show config file existing info for debug

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,6 +1,7 @@
 set -e
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
     ssh -i deploy/ssh $DEPLOY_ADDR "cd /home/snoin/snoin.com && git pull -f origin master"
+    ssh -i deploy/ssh $DEPLOY_ADDR "ls /home/snoin/snoin.com/snoin.cfg.py"
     ssh -i deploy/ssh $DEPLOY_ADDR "cd /home/snoin/snoin.com && rm -rf node_modules && npm install && npm run build"
     ssh -i deploy/ssh $DEPLOY_ADDR $DEPLOY_SERVER_RESTART
 fi
