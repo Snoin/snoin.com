28606c342bfe6ddeaa5ebe0a5dfcae380a32497a
Kim Jin Su <item4_hun@hotmail>
2015-11-24 17:31:06 +0900

    Move commands to deploy.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,5 +1,9 @@
 set -e
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+    openssl aes-256-cbc -K $encrypted_ff646b1048a4_key -iv $encrypted_ff646b1048a4_iv -in ssh.tar.enc -out ssh.tar -d
+    file ssh.tar
+    tar -xvf ssh.tar
+    ls -l deploy/
     ssh -i deploy/ssh $DEPLOY_ADDR "cd /home/snoin/snoin.com && git pull -f origin master"
     ssh -i deploy/ssh $DEPLOY_ADDR "ls /home/snoin/snoin.com/snoin.cfg.py"
     ssh -i deploy/ssh $DEPLOY_ADDR "cd /home/snoin/snoin.com && rm -rf node_modules && npm install && npm run build"
