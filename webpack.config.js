ac10c3a8af4a5874e4ad79a2b8339ea139faf597
Kim Jin Su <item4_hun@hotmail.com>
2015-12-13 06:04:06 +0900

    Apply reindent and homogenize style

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,44 @@
 var webpack = require("webpack");
 var path = require('path');
+
 module.exports = {
-    context: __dirname + "/snoin/web/static",
-    entry: './src/main.js',
-    output: {
-        path: path.join(__dirname, "snoin", "web", "static", "dist"),
-        publicPath: "/static/dist/",
-        filename: 'app.js'
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                loader: 'babel',
-                query: {
-                    presets: ['es2015', 'stage-0']
-                }
-            },
-            { test: /\.s?css$/, loaders: ['style', 'css', 'sass'] },
-            { test: /\.(eot|woff2?|ttf|svg)(\?.+)?$/, loader: 'file?name=[name].[ext]'  },
-            { test: /\.(jpe?g|png|gif)$/, 'loader': 'file?name=[name].[ext]' }
-        ]
-    },
-    plugins: [
-        new webpack.ProvidePlugin({
-            $: "jquery",
-            jQuery: "jquery",
-            "window.jQuery": "jquery"
-        })
-    ],
-    devtool: 'source-map'
+  context: __dirname + "/snoin/web/static",
+  entry: './src/main.js',
+  output: {
+    path: path.join(__dirname, "snoin", "web", "static", "dist"),
+    publicPath: "/static/dist/",
+    filename: 'app.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'stage-0']
+        }
+      },
+      {
+        test: /\.s?css$/,
+        loaders: ['style', 'css', 'sass']
+      },
+      {
+        test: /\.(eot|woff2?|ttf|svg)(\?.+)?$/,
+        loader: 'file?name=[name].[ext]'
+      },
+      {
+        test: /\.(jpe?g|png|gif)$/,
+        loader: 'file?name=[name].[ext]'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+      "window.jQuery": "jquery"
+    })
+  ],
+  devtool: 'source-map'
 };
