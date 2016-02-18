b0181e142eab12f1c2f804d93aae0ea11ccec01c
Kim Jin Su <item4_hun@hotmail.com>
2016-02-19 00:20:00 +0900

    Make service javascript base

--- snoin/web/static/src/main.js
+++ snoin/web/static/src/main.js
@@ -20,6 +20,8 @@ const $navbar = $('#menu');
 const $window = $(window);
 const docElem = document.documentElement;
 const changeHeaderOn = 300;
+const $message = $('#message');
+const $serviceForm = $('#serviceForm');
 let didScroll = false;
 
 function scrollTop() {
@@ -44,8 +46,57 @@ function initScroll() {
   });
 }
 
+function updateMessage() {
+  const $visitant = $serviceForm.find('input[name=visitant]:checked');
+  const $maxConnection = $serviceForm.find('input[name=max_connection]:checked');
+  const $userEnv = $serviceForm.find('input[name=user_env]:checked');
+  const $supportMobile = $serviceForm.find('input[name=support_mobile]:checked');
+  const $feature = $serviceForm.find('input[name=feature]:checked');
+  const $price = $serviceForm.find('input[name=price]:checked');
+  const $design = $serviceForm.find('input[name=design]:checked');
+  const $maintenance = $serviceForm.find('input[name=maintenance]:checked');
+  const $maintenanceCompany = $serviceForm.find('input[name=maintenance_company]:checked');
+
+  let message = '';
+  if ($visitant.length) {
+    message += `예상 일일 방문자: ${$visitant.val()}\n`;
+  }
+  if ($maxConnection.length) {
+    message += `예상 최대 동시 접속: ${$maxConnection.val()}\n`;
+  }
+  if ($userEnv.length) {
+    message += `주요 방문 계층 환경: ${$userEnv.val()}\n`;
+  }
+  if ($supportMobile.length) {
+    message += `모바일 지원 여부: ${$supportMobile.val()}\n`;
+  }
+  if ($feature.length) {
+    $feature.each(function callback() {
+      message += `원하는 기능: ${$(this).val()}\n`;
+    });
+  }
+  if ($price.length) {
+    message += `희망 가격대: ${$price.val()}\n`;
+  }
+  if ($design.length) {
+    message += `디자이너: ${$design.val()}\n`;
+  }
+  if ($maintenance.length) {
+    message += `희망 유지보수 비용: ${$maintenance.val()}\n`;
+  }
+  if ($maintenanceCompany.length) {
+    message += `유지보수 방법: ${$maintenanceCompany.val()}\n`;
+  }
+  if (message) {
+    message += '\n\n-------\n\n';
+  }
+  $message.val(message);
+  return true;
+}
+
 $(document).ready(() => {
   const $form = $('#contactForm');
+  const $serviceFormInputs = $('#serviceForm input');
 
   initScroll();
   scrollPage();
@@ -73,8 +124,24 @@ $(document).ready(() => {
   });
 
 
+  $serviceFormInputs.change(e => {
+    const $this = $(e.currentTarget);
+    const type = $this.attr('type');
+    const name = $this.attr('name');
+
+    if (type === 'checkbox') {
+      $this.parent().toggleClass('checked');
+    } else {
+      $(`#serviceForm input[name=${name}]`).parent().removeClass('checked');
+      $this.parent().addClass('checked');
+    }
+
+    updateMessage();
+    return true;
+  });
+
+
   $form.submit(e => {
-    const $message = $('#message');
     const name = $('#name').val();
     const email = $('#email').val();
     const phone = $('#phone').val();
@@ -85,6 +152,7 @@ $(document).ready(() => {
     function onSuccess(data) {
       window.alert(data.message);
       $message.val('');
+      updateMessage();
     }
 
     function onFail(error) {
